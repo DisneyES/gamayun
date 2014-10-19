9cd037469ca840c551384cd75f71eb2f35f44751
Gawyn <me@cristianplanas.com>
2014-10-20 00:32:58 +0200

    Temp

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -4,7 +4,7 @@ class CoverUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -14,6 +14,14 @@ class CoverUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :main_cover_element do
+    process :resize_to_fit => [1180, 664]
+  end
+
+  version :cover_element do
+    process :resize_to_fit => [580, 326]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
