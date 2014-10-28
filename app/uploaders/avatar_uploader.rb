dd7e1c54f2531e2a1ef555a91576e551c17450d6
Gawyn <me@cristianplanas.com>
2014-10-28 19:18:28 +0100

    Author image in articles#show

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,6 +5,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  include ImageUtils
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -15,6 +16,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  process :fit_in_size => [110, 110]
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
