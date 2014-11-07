062019176dcdc9757bc4147bf7ea0b6d1bc78453
Cristian Planas <cplanas@zendesk.com>
2014-11-07 17:10:20 +0100

    Setting url in content

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,3 +1,11 @@
 class Content < ActiveRecord::Base
   belongs_to :author, class_name: User
+
+  before_validation :set_url!, on: :create
+
+  private
+
+  def set_url!
+    self.url ||= title.parameterize
+  end
 end
