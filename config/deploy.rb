934bcd20ed79ce3f80d99b362e2758af61ae5052
Gawyn <me@cristianplanas.com>
2014-11-10 23:46:55 +0100

    Adding config/s3.yml to linked dirs in deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,7 @@ set :linked_files, %w{config/database.yml config/secrets.yml .ruby-version .ruby
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{ log tmp/cache }
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
