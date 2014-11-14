2f01ee778905d661c10ee27d876fda7e7fa62f76
Gawyn <me@cristianplanas.com>
2014-11-14 18:09:45 +0100

    Only published articles are accessible

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @articles = Article.order("created_at desc").limit(5)
+    @articles = Article.published.order("created_at desc").limit(5)
   end
 end
