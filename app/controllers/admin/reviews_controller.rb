2a8c218c305132d1e6d3840c617ee1660138f25b
Gawyn <me@cristianplanas.com>
2014-11-14 18:36:21 +0100

    Better management of reviews

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,4 +1,8 @@
 class Admin::ReviewsController < Admin::AdminController
+  def index
+    @reviews = Review.all
+  end
+
   def new
     @review = Review.new
   end
@@ -7,12 +11,23 @@ class Admin::ReviewsController < Admin::AdminController
     @review = Review.new review_params
     @review.author = current_user
     @review.save
-    redirect_to @review
+    redirect_to admin_reviews_path
+  end
+
+  def edit
+    @review = Review.where(url: params[:id]).limit(1).first
+  end
+
+  def update
+    @review = Review.where(url: params[:id]).limit(1).first
+
+    @review.update article_params
+    redirect_to admin_reviews_path
   end
 
   private
   
   def review_params
-    params.require(:review).permit(:title, :body)
+    params.require(:review).permit(:title, :body, :published)
   end
 end
