b5dc15ce9e72cffed8e4e43b8a370c91cf17e97e
Gawyn <me@cristianplanas.com>
2015-02-16 19:41:23 +0100

    Section model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141116132502) do
+ActiveRecord::Schema.define(version: 20150216183915) do
 
   create_table "contents", force: true do |t|
     t.string   "title"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20141116132502) do
     t.string   "url"
     t.boolean  "published",  default: false
     t.string   "poster"
+  end
+
+  create_table "sections", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
